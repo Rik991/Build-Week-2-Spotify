60840b61cb7d0049e812d78805d9011fe8265aae
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2024-10-01 19:24:32 +0200

    media-player-funzioni-principali
    creazione funzioni per il controllo dell'audio e registrazione nel localstorage

--- script/index.js
+++ script/index.js
@@ -6,6 +6,7 @@ const footerImg = document.getElementById("footer-img");
 const duration = document.getElementById("duration");
 const currentAudio = document.getElementById("current-audio");
 const footerPlayBtn = document.querySelector(".footerPlay");
+const currentAudioTime = document.getElementById("current-time");
 
 const hiddenAsideBtn = document.querySelector(".bi-view-list");
 hiddenAsideBtn.addEventListener("click", () => {
@@ -23,7 +24,7 @@ const getData = (url) => {
       }
     })
     .then((albums) => {
-      console.log("album disponibili", albums);
+      //   console.log("album disponibili", albums);
       const albArray = albums.data;
       const element = albArray[Math.floor(Math.random() * 25)];
       const song = new Songs(element.artist.name, element.title_short, element.preview, element.album.cover, element.duration);
@@ -129,7 +130,7 @@ const artisti = [
   "lady",
   "adele",
   "guns",
-  "greenday"
+  "greenday",
 ];
 
 getData(genericUrl + artisti[Math.floor(Math.random() * artisti.length)]);
@@ -151,7 +152,9 @@ class Songs {
   }
 }
 
-//funzione pulsante Play nel footer
+// funzioni media player
+
+// pulsante Play nel footer
 
 footerPlayBtn.addEventListener("click", () => {
   if (currentAudio.paused) {
@@ -162,3 +165,45 @@ footerPlayBtn.addEventListener("click", () => {
     footerPlayBtn.textContent = "Play";
   }
 });
+
+const progress = document.getElementById("progress-bar");
+const volume = document.getElementById("volume");
+console.log(volume.value);
+
+volume.addEventListener("input", () => {
+  currentAudio.volume = volume.value / 100;
+  console.log(currentAudio.volume);
+});
+
+progress.addEventListener("input", () => {
+  currentAudio.currentTime = progress.value;
+  console.log(currentAudio.currentTime);
+});
+
+const time = localStorage.getItem("timeAudio");
+const footerImgServer = localStorage.getItem("footerImgServer");
+const currentAudioServer = localStorage.getItem("currentAudioServer");
+const currentArtisServer = localStorage.getItem("currentArtisServer");
+const currentNameSongServer = localStorage.getItem("currentNameSongServer");
+
+console.log(time);
+
+if (time) {
+  currentAudio.currentTime = time;
+  footerImg.src = footerImgServer;
+  currentAudio.src = currentAudioServer;
+  artistSong.innerText = currentArtisServer;
+  nameSong.innerText = currentNameSongServer;
+}
+
+setInterval(registra, 1000);
+
+function registra() {
+  localStorage.setItem("timeAudio", currentAudio.currentTime);
+  localStorage.setItem("footerImgServer", footerImg.src);
+  localStorage.setItem("currentAudioServer", currentAudio.src);
+  localStorage.setItem("currentArtisServer", artistSong.innerText);
+  localStorage.setItem("currentNameSongServer", nameSong.innerText);
+  currentAudioTime.innerText = Math.round(currentAudio.currentTime);
+  progress.value = currentAudio.currentTime;
+}
