5fa5447e1458a80ba9e0041af9b4e0838ede5dc6
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2024-10-01 10:31:39 +0200

    inizio js

--- script/index.js
+++ script/index.js
@@ -1,1 +1,20 @@
+const genericUrl = " https://striveschool-api.herokuapp.com/api/deezer/search?q=tizianoferro";
 
+const getData = () => {
+  fetch(genericUrl)
+    .then((response) => {
+      if (response.ok) {
+        console.log(response);
+        return response.json();
+      } else {
+        throw new Error("Errore nel recupero dei dati");
+      }
+    })
+    .then((albums) => {
+      console.log("album disponibili", albums);
+    })
+    .catch((err) => {
+      console.log("ERROR", err);
+    });
+};
+getData();
