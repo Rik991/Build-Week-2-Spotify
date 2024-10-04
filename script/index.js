3e579fbc51e2959d935939f19c2ec96865ee8685
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2024-10-04 12:12:36 +0200

    fix-butonHome-position-ab+hero-fix

--- script/index.js
+++ script/index.js
@@ -76,7 +76,7 @@ const getData = (url) => {
                             width="30"
                             height="30"
                             fill="#63D566"
-                            class="bi bi-play-circle-fill position-absolute"
+                            class="bi bi-play-circle-fill position-absolute translate-middle-y"
                             viewBox="0 0 16 16"
                           >
                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z" />
